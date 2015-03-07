9cd7fd92164b2860c35038d9938ab42f25d6ea0c
Jay Hesselberth <jay.hesselberth@gmail.com>
2015-03-07 14:27:51 -0700

    rm org and pi tables; too big to work with for now

--- R/data.R
+++ R/data.R
@@ -4,34 +4,14 @@
 #'
 #' projects
 #'
-#' @format A data frame with five variables: \code{year}, \code{sex},
-#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
-#'   of applicants in that year).
+#' @format A data frame with nine variables: \code{application.id}, \code{institute},
+#'   \code{activity}, \code{project.num}, \code{fiscal.year}, \code{project.start},
+#'   \code{project.end}, \code{study.section}, \code{total.cost} 
 #'
 #' For further details, see \url{http://exporter.nih.gov/ExPORTER_Catalog.aspx?sid=4&index=0}
 "projects"
 
-#' project.pis 
-#'
-#' The SSA baby names data comes from social security number (SSN) applications.
-#' SSA cards were first issued in 1936, but were only needed for people with
-#' an income. In 1986, the law changed effectively requiring all children to
-#' get an SSN at birth.
-#'
-#' @format A data frame with 3 variables: \code{year}, \code{sex} and
-#'   \code{applicants}
-"project.pis"
-
-#' project.orgs 
-#'
-#' Information about funded organizations.
-#'
-#' @format A data frame with 4 variables: \code{org.city},
-#'   \code{org.country}, \code{org.name} and \code{org.state}
-"project.orgs"
-
-
-#' Publinks
+#' publinks
 #'
 #' Links Pubmed IDs to Project IDs. 
 #'
