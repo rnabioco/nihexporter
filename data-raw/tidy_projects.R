b93b3155b191c65f224cc98fda4d2000379094d9
Jay Hesselberth <jay.hesselberth@gmail.com>
2015-03-07 14:28:29 -0700

    rm org and pi tables; trim down projects table to fit under github limit (100M)

--- data-raw/tidy_projects.R
+++ data-raw/tidy_projects.R
@@ -13,7 +13,7 @@ csvfiles <- dir(path, pattern = '\\.csv', full.names = TRUE)
 
 tables <- lapply(csvfiles, read.csv, header = TRUE)
 
-projects.tbl <- rbindlist(tables)
+projects.tbl <- rbindlist(tables, fill = TRUE)
 projects.tbl <- tbl_df(projects.tbl)
 
 # coerce colnames
@@ -22,30 +22,36 @@ names(projects.tbl) <- names(projects.tbl) %>%
   str_replace_all('_','.')
 
 # org table
-project.orgs <- projects.tbl %>%
-  select(org.city:org.state) %>%
-  select(-(org.dept:org.fips)) %>%
-  distinct()
-save(project.orgs, file = 'data/project.orgs.rdata')
+#project.orgs <- projects.tbl %>%
+#  select(org.city:org.state) %>%
+#  select(-(org.dept:org.fips)) %>%
+#  distinct() %>%
+#  mutate(org.id = seq_along(org.name))
+#save(project.orgs, file = 'data/project.orgs.rdata')
 
-# project.pis table
-project.pis <- projects.tbl %>%
-  select(application.id, pi.names) %>%
-  separate(pi.names, into = c(1:20), sep = ';', extra = 'drop') %>%
-  gather(application.id) %>%
-  setNames(c('application.id', 'pi.num', 'pi.name')) %>%
-  filter(!is.na(pi.name)) %>%
-  select(application.id, pi.name) %>%
-  mutate(application.id = as.factor(application.id))
-save(project.pis, file = 'data/project.pis.rdata')
+# project.pis table - select the last PI from the list
+#project.pis <- projects.tbl %>%
+#  select(application.id, pi.names) %>%
+#  separate(pi.names, into = c(1:20), sep = ';', extra = 'drop') %>%
+#  gather(application.id) %>%
+#  setNames(c('application.id', 'pi.num', 'pi.name')) %>%
+#  filter(!is.na(pi.name)) %>%
+#  group_by(application.id) %>%
+#  arrange(desc(pi.num)) %>%
+#  slice(1:1) %>%
+#  select(application.id, pi.name) %>%
+#  mutate(application.id = as.factor(application.id))
+#save(project.pis, file = 'data/project.pis.rdata')
 
 # projects table
 projects <- projects.tbl %>%
-  select(application.id, activity, core.project.num, full.project.num,
-         fy, org.name, project.start, project.end,
-         study.section, support.year, total.cost, total.cost.sub.project) %>%
-  rename(project.num = core.project.num) %>%  
+  select(application.id, administering.ic, activity,
+         core.project.num, fy,
+         project.start, project.end,
+         study.section, total.cost) %>%
+  rename(project.num = core.project.num,
+         fiscal.year = fy,
+         institute = administering.ic) %>%  
   mutate(project.end = mdy(project.end),
-         project.start = mdy(project.start),
-         support.year = factor(support.year))
-save(project, file = 'data/projects.rdata')         
+         project.start = mdy(project.start))
+save(projects, file = 'data/projects.rdata')         
