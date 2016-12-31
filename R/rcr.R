c38ed81b6461948f976863b94f0792da4dd9b10a
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-12-31 08:36:56 -0700

    clarify max pmids

--- R/rcr.R
+++ R/rcr.R
@@ -1,7 +1,7 @@
 #' Retrieve Relative Citation Ratio (RCR) for PubMed IDs.
 #'
-#' @param pmids vector of PubMed IDs to retrieve
-#' 
+#' @param pmids vector of PubMed IDs to retrieve (max of 1000 at a time)
+#'
 #' @return a data_frame the following variables:
 #' \describe{
 #' \item{\code{pmid}}{PubMed ID}
@@ -16,34 +16,34 @@
 #' \item{\code{relative_citation_ratio}}{RCR}
 #' \item{\code{title}}{article title}
 #' \item{\code{year}}{publication year}
-#' } 
-#'  
+#' }
+#'
 #' @import httr
 #' @import jsonlite
 #' @import dplyr
-#' 
+#'
 #' @seealso \url{https://icite.od.nih.gov/api}
-#' 
-#' @examples 
+#'
+#' @examples
 #' pmids <- c(26001965, 25015380)
 #' rcr(pmids)
-#' 
+#'
 #' @export
 rcr <- function(pmids) {
 
-  max_pmids <- 1000 
+  max_pmids <- 1000
   if(length(pmids) > max_pmids)
     stop('maximum pubmed ids exceeded', call. = FALSE)
-  
-  ## icite url 
+
+  ## icite url
   url <- 'https://icite.od.nih.gov'
   path <- 'api/pubs'
   query <- list('pmids' = paste0(pmids, collapse = ','))
-  
+
   url_built <- modify_url(url, path = path, query = query)
-  
+
   raw <- GET(url_built) %>% content(., 'text') %>% fromJSON(.)
-  
-  as_data_frame(raw$data) 
+
+  as_data_frame(raw$data)
 }
 
