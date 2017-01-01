ada5f4eee44975e7f71eaabed39ae95a26b2e821
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-01-01 06:47:52 -0700

    table integrity tests

---
+++ tests/testthat/test-tables.r
@@ -0,0 +1,12 @@
+context('table integrity')
+
+test_that('table rows are unique', {
+  expect_equal(nrow(org_info), nrow(unique(org_info)))
+  expect_equal(nrow(patents), nrow(unique(patents)))
+  expect_equal(nrow(project_io), nrow(unique(project_io)))
+  expect_equal(nrow(project_orgs), nrow(unique(project_orgs)))
+  expect_equal(nrow(project_pis), nrow(unique(project_pis)))
+  expect_equal(nrow(projects), nrow(unique(projects)))
+  expect_equal(nrow(publications), nrow(unique(publications)))
+  expect_equal(nrow(publinks), nrow(unique(publinks)))
+})
