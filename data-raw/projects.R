9ac0de93d5e28c3de65690f4177c5bf1973154a7
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-01-07 05:51:44 -0700

    move org stuff to new file

--- data-raw/projects.R
+++ data-raw/projects.R
@@ -1,9 +1,4 @@
-# projects table
-#' load PROJECTS CSV files and create these tables:
-#' - projects
-#' - project_orgs
-#' - project_pis
-#' - org_info
+## projects and project_pis tables
 
 source('data-raw/common.R')
 
@@ -32,22 +27,6 @@ col_types <- cols_only(
 
 projects.tbl <- load_tbl(path, col_types)
 
-# org table - link on org.duns
-org_info <- projects.tbl %>%
-  # hack to remove incorrect DUNS in 2008 and prior
-  filter(fy >= 2009) %>%
-  select(org.city, org.state, org.duns, org.name) %>%
-  filter(!is.na(org.duns)) %>%
-  mutate(org.duns = str_trim(org.duns),
-         org.duns = str_replace(org.duns, '^0+', ''),
-         org.duns = as.numeric(org.duns),
-         org.state = as.factor(org.state),
-         org.city = as.factor(org.city)) %>%
-  distinct() %>%
-  arrange(org.duns)
-
-use_data(org_info, compress = 'xz')
-
 projects <- projects.tbl %>%
   select(application.id,
          administering.ic, activity,
@@ -74,44 +53,14 @@ projects <- projects.tbl %>%
          fy.cost = as.double(total.cost)) %>%
   select(-total.cost)
 
-use_data(projects, compress = 'xz')
-
-#' load fixes for DUNS numbers and parse here
-duns_path <- 'data-raw//DUNS_FIX'
-duns_col_types <- cols(
+## load the FY2985-1999 funding data
+col_types <- cols_only(
   APPLICATION_ID = col_integer(),
-  ORG_DUNS = col_character()
+  FY = col_integer(),
+  TOTAL_COST = col_double()
 )
 
-duns.tbl <- load_tbl(duns_path, duns_col_types)
-
-# now join the DUNS numbers. we want DUNS from the new tables from 2000-2008 (those are WRONG in the project.tbl),
-# and the DUNS from 2009 onward are correct in the projects.tbl
-old_duns <- duns.tbl %>%
-  left_join(projects, by = 'application.id') %>%
-  select(application.id, org.duns)
-
-new_duns <- projects.tbl %>%
-  filter(fy >= 2009) %>%
-  select(application.id, org.duns)
-
-all_duns <- bind_rows(old_duns, new_duns)
-
-project_orgs <- all_duns %>%
-  separate(org.duns, into = as.character(c(1:20)), sep = ';', extra = 'drop') %>%
-  gather(application.id) %>%
-  setNames(c('application.id','count','org.duns')) %>%
-  select(application.id, org.duns) %>%
-  filter(org.duns != '') %>%
-  mutate(org.duns = str_trim(org.duns),
-         org.duns = str_replace(org.duns, '^0+', ''),
-         org.duns = as.numeric(org.duns)) %>%
-  arrange(application.id) %>%
-  unique()
-
-use_data(project_orgs, compress = 'xz')
-
-# project_pis table
+## project_pis table
 project_pis <- projects.tbl %>%
   select(core.project.num, pi.ids, administering.ic) %>%
   rename(project.num = core.project.num,
