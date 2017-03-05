033281104cb65132b454ea82404464d898dc9afe
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-03-05 08:25:41 -0700

    update suggests and namespace

--- R/vars.R
+++ R/vars.R
@@ -1,11 +1,12 @@
+#' @importFrom stats setNames
 globalVariables(".")
 
 #' Two-letter acronyms for NIH institutes
-#' 
+#'
 #' @source \url{http://grants.nih.gov/grants/acronym_list.htm}
-#' 
+#'
 #' @export
-#' 
-nih.institutes <- c("AA", "AG", "AI", "AR", "AT", "CA", "DA", "DC", "DE", "DK", "EB", "ES", "EY", "GM", "HD", "HG", "HG", 
-    "HL", "LM", "MD", "MH", "NR", "NS", "RM", "RR", "TR", "TW") 
+#'
+nih.institutes <- c("AA", "AG", "AI", "AR", "AT", "CA", "DA", "DC", "DE", "DK", "EB", "ES", "EY", "GM", "HD", "HG", "HG",
+    "HL", "LM", "MD", "MH", "NR", "NS", "RM", "RR", "TR", "TW")
 
