b1f313a0ad476784a407efe6e06a7ddd57ae2475
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-02-01 15:43:11 -0700

    Add test

--- tests/testthat/test-rcr.r
+++ tests/testthat/test-rcr.r
@@ -1,5 +1,11 @@
 context('rcr')
 
+test_that("rcr() returns expected", {
+  pmids <- c(26001965, 25015380)
+  res <- rcr(pmids)
+  expect_equal(res$relative_citation_ratio, c(2.16, 0.94))
+})
+
 test_that('too many PMIDs throws an error', {
   # max is currenty 1000
   fake <- 1:1001
