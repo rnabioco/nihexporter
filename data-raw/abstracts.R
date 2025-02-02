e075413a007888654fb836e94f4a0833c2e420f6
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-02-02 13:35:39 -0700

    Tokenize abstracts
    This creates a large tibble, 4M rows.

--- data-raw/abstracts.R
+++ data-raw/abstracts.R
@@ -1,10 +1,7 @@
 # parse and tokenize abstracts
 
-# https://juliasilge.com/blog/animal-crossing/
-# https://textrecipes.tidymodels.org/articles/cookbook---using-more-complex-recipes-involving-text.html
-
-library(tidymodels)
-library(textrecipes)
+library(tidyverse)
+library(tidytext)
 library(here)
 
 source("data-raw/common.R")
@@ -19,8 +16,52 @@ col_types <- cols_only(
 abstracts_raw_tbl <-
   load_tbl(path, col_types) |>
     left_join(projects, by = "application_id") |>
-    select(application_id, fiscal_year, institute, abstract_text) |>
+    select(activity, fiscal_year, institute, abstract_text) |>
+    # extramural only
+    filter(!str_detect(activity, "^Z")) |>
     na.omit() |>
     unique()
 
-# test tokenize
+data(stop_words)
+custom_stop_words <- tibble(
+  word = c(
+    # generic to abstracts
+    "research",
+    "specific",
+    "studies",
+    "aim",
+    # meaningless annotations
+    "description",
+    "unreadable"
+  )
+)
+
+tokenize_words <- function(df) {
+  unnest_tokens(df, input = abstract_text, output = word) |>
+    # remove words that are numbers
+    filter(!str_detect(word, "^[0-9]*$")) |>
+    anti_join(stop_words) |>
+    anti_join(custom_stop_words) |>
+    count(activity, fiscal_year, institute, word, sort = TRUE) |>
+    filter(n >= 10)
+}
+
+df_splits <- group_by(abstracts_raw_tbl, fiscal_year, institute) |>
+  group_split()
+
+# df_splits <- df_splits[1:10]
+
+library(furrr)
+library(progressr)
+plan(multisession, workers = 12)
+with_progress({
+  p <- progressor(steps = length(df_splits))
+
+  abstract_words <- future_map_dfr(
+    df_splits,
+    ~{
+      p()
+      tokenize_words(.x)
+    }
+  )
+})
