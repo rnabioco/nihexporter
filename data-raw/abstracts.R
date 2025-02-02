67a054cbeb460d820062fb616c7e9f0c6c35ed56
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-02-02 11:39:44 -0700

    More sentiment

--- data-raw/abstracts.R
+++ data-raw/abstracts.R
@@ -1,5 +1,8 @@
 # parse and tokenize abstracts
 
+# https://juliasilge.com/blog/animal-crossing/
+# https://textrecipes.tidymodels.org/articles/cookbook---using-more-complex-recipes-involving-text.html
+
 library(tidymodels)
 library(textrecipes)
 library(here)
@@ -13,8 +16,11 @@ col_types <- cols_only(
   ABSTRACT_TEXT = col_character(),
 )
 
-abstracts_raw_tbl <- load_tbl(path, col_types) |>
-  left_join(projects, by = "application_id") |>
-  select(application_id, fiscal_year, abstract_text) |>
-  na.omit() |>
-  unique()
+abstracts_raw_tbl <-
+  load_tbl(path, col_types) |>
+    left_join(projects, by = "application_id") |>
+    select(application_id, fiscal_year, institute, abstract_text) |>
+    na.omit() |>
+    unique()
+
+# test tokenize
